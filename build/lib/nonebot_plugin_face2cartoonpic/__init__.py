861d9591bdb1df343592bb22e83b129b7d918fa7
ANGJustinl <96008766+ANGJustinl@users.noreply.github.com>
2022-12-27 09:48:17 +0800

    v0.2.0

---
+++ build/lib/nonebot_plugin_face2cartoonpic/__init__.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+from requests import get
+
+import nonebot
+from nonebot.typing import T_State
+from nonebot import get_driver,on_command,on_regex
+from nonebot.matcher import Matcher
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, Message, MessageSegment,MessageEvent
+from nonebot.adapters.onebot.v11.helpers import extract_image_urls
+from nonebot.params import Arg
+from nonebot.log import logger
+
+from .config import Config
+from .limiter import limiter
+from .tencentapi import get_pic
+
+
+fc = on_command('人像变换',aliases={'facechange','fc'}, priority=5, block=True)
+
+
+@fc.handle()
+async def image2comic(event: MessageEvent, matcher: Matcher):
+    message = reply.message if (reply := event.reply) else event.message
+    if imgs := message["image"]:
+        matcher.set_arg("imgs", imgs)
+   
+@fc.got("imgs", prompt="请发送用于启动的原图")
+async def get_image(state: T_State, imgs: Message = Arg()):
+    urls = extract_image_urls(imgs)
+    if not urls:
+        await fc.finish("没有找到图片,请重新启动并在启动语句后添加图片")
+        
+    state["urls"] = urls
+
+
+@fc.handle()
+async def _handle(matcher: Matcher,event: MessageEvent,state: T_State):
+    
+    user_id = event.user_id
+    if not limiter.check(user_id):
+        left_time = limiter.left_time(user_id)
+        await matcher.finish(f'我知道你急了.但是你先别急,cd还有{left_time}秒')
+        return 
+    url_input = state["urls"][0]
+    limiter.start_cd(user_id)
+    
+    msg_raw = await get_pic(url_input)
+
+    if msg_raw == 1:
+        logger.error("画图失败")
+        await fc.finish("寄，画图失败了,没有设置Secret_key", reply_message=True) 
+    if msg_raw == 2:
+        logger.error("画图失败")
+        await fc.finish("寄，画图失败了,输入的不是人像", reply_message=True)
+
+    url = msg_raw
+    await fc.send(url)
+    await fc.finish(MessageSegment.image(file=url, cache=False), at_sender=True)
+
+
+
+_sub_plugins = set()
+_sub_plugins |= nonebot.load_plugins(
+    str((Path(__file__).parent / "plugins").
+    resolve()))
+
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as f:
+    long_description = f.read()
+
+# Package meta-data.
+NAME = 'nonebot_plugin_face2cartoonpic'
+DESCRIPTION = '基于 腾讯云合成图的 以图绘图 NoneBot2插件'
+URL = 'https://github.com/ANGJustinl/nonebot_plugin_face2cartoonpic'
+EMAIL = 'angjustin@163.com'
+AUTHOR = 'ANGJustinl'
+VERSION = '0.2.1'
+
+setuptools.setup(
+    name="nonebot_plugin_face2cartoonpic",
+    version="0.2.1",
+    author="ANGJustinl",
+    author_email="angjustin@163.com",
+    keywords=["pip", "nonebot2", "nonebot", "nonebot_plugin"],
+    description="""基于 腾讯云合成图的 以图绘图 NoneBot2插件""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ANGJustinl/nonebot_plugin_face2cartoonpic",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        'nb-cli>=0.6.7'
+        'pydantic>=1.9.2'        
+    ],
+    python_requires=">=3.8"
+)
